1a4b068bf28f50a8998a32699affe2d02ac70d48
chrs-m <mister.ch@gmail.com>
2021-10-13 10:42:34 +0200

    moved data to own file (data.php)

---
+++ PHP/data.php
@@ -0,0 +1,26 @@
+<?php
+
+$drivers = [
+    ['name' => 'Lewis Hamilton', 'team' => 'Mercedes-AMG Petronas F1 Team', 'birthYear' => 1985, 'wins' => 100],
+    ['name' => 'Valtteri Bottas', 'team' => 'Mercedes-AMG Petronas F1 Team', 'birthYear' => 1989, 'wins' => 10],
+    ['name' => 'Max Verstappen', 'team' => 'Red Bull Racing Honda', 'birthYear' => 1997, 'wins' => 17],
+    ['name' => 'Sergio Perez', 'team' => 'Red Bull Racing Honda', 'birthYear' => 1990, 'wins' => 2],
+    ['name' => 'Daniel Ricciardo', 'team' => 'McLaren F1 Team', 'birthYear' => 1989, 'wins' => 8],
+    ['name' => 'Lando Norris', 'team' => 'McLaren F1 Team', 'birthYear' => 1999, 'wins' => 0],
+    ['name' => 'Charles Leclerc', 'team' => 'Scuderia Ferrari Mission Winnow', 'birthYear' => 1997, 'wins' => 2],
+    ['name' => 'Carlos Sainz', 'team' => 'Scuderia Ferrari Mission Winnow', 'birthYear' => 1994, 'wins' => 0],
+
+];
+
+$teams = [
+    ['name' => 'Mercedes-AMG Petronas F1 Team', 'year' => 1970, 'drivers' => 'Lewis Hamilton & Valtteri Bottas', 'Championchips' => 7, 'Team principal' => 'Toto Wolff'],
+    ['name' => 'Red Bull Racing Honda', 'year' => 1997, 'drivers' => 'Max Verstappen & Sergio Perez', 'Championchips' => 4, 'Team principal' => 'Christian Horner'],
+    ['name' => 'McLaren F1 Team', 'year' => 1966, 'drivers' => 'Daniel Ricciardo & Lando Norris', 'Championchips' => 8, 'Team principal' => 'Andreas Seidl'],
+    ['name' => 'Scuderia Ferrari Mission Winnow', 'year' => 1950, 'drivers' => 'Charles Leclerc & LCarlos Sainz', 'Championchips' => 16, 'Team principal' => 'Mattia Binotto'],
+];
+
+
+$teamsWithMultipleWinners = [
+    'Mercedes-AMG Petronas F1 Team',
+    'Red Bull Racing Honda',
+];
