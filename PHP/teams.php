37b058bb2e9933b120b96a38a003dfe6e931a336
chrs-m <mister.ch@gmail.com>
2021-10-18 10:36:34 +0200

    Fixed driversFromTeam function

--- PHP/teams.php
+++ PHP/teams.php
@@ -5,14 +5,18 @@
 <div class="mb-20">
     <main class="grid grid-cols-2 gap-1">
         <?php foreach ($teams as $team) : ?>
+            <?php
+            $driversFromTeam = getDriversFromTeam($drivers, $team['name']);
+            $driversString = implode(", ", $driversFromTeam);
+            ?>
             <article class="grid justify-items-center mb-20 sm:text-xs">
                 <img class="scale-75 motion-safe:hover:scale-90" src="<?php echo $team['image']; ?>" alt="<?php echo $team['name']; ?>">
                 <h2 class="uppercase font-bold sm:text-lg text-xs text-center"><?php echo $team['name']; ?></h2>
                 <div class="flex flex-col items-center justify-center justify-items-center">
+                    <p class="sm:text-lg text-xs text-center"><?php echo "Drivers: " . $driversString; ?></p>
                     <p class="sm:text-lg text-xs"><?php echo "Team Principal: " . $team['teamPrincipal']; ?></p>
                     <p class="sm:text-lg text-xs"><?php echo "Championchips: " . $team['championchips']; ?></p>
                 </div>
-
             </article>
         <?php endforeach; ?>
     </main>
